f04dd31eda0187790bd888dfae53a4a12247cd71
neuqs0035 <152640221+neuqs0035@users.noreply.github.com>
2023-12-30 12:45:01 +0530

    SPELLING MISTAKE CORRECTED IN TITLE WHILE SENDING NOTIFICATION

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ data = requests.get("https://api.adviceslip.com/advice")
 
 while True:
     if data.status_code == 200:
-        notification.notify(title="Adive",message=data.json()["slip"]["advice"],timeout=10)
+        notification.notify(title="Advice",message=data.json()["slip"]["advice"],timeout=10)
         sleep(7200)
